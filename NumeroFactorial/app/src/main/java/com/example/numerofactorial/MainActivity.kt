b28a11330e5e51db03bf9c245dfc8e999cfd5cfa
VersatileAnerst <alarconbasc@gmail.com>
2025-10-08 13:50:24 +0200

    Actualizar Factorial

--- NumeroFactorial/app/src/main/java/com/example/numerofactorial/MainActivity.kt
+++ NumeroFactorial/app/src/main/java/com/example/numerofactorial/MainActivity.kt
@@ -15,14 +15,21 @@ class MainActivity : AppCompatActivity() {
 
         val numero: EditText = findViewById(R.id.editTextText)
         val boton: Button = findViewById(R.id.button)
-        val texto: TextView = findViewById(R.id.textView)
+        val textoResultado: TextView = findViewById(R.id.textView)
         boton.setOnClickListener {
             val numeroResultado = numero.text.toString().toIntOrNull()
-            CalcularFactorial(numeroResultado)
+            if(numeroResultado!= null)
+                textoResultado.text=CalcularFactorial(numeroResultado).toString()
+            else {
+                textoResultado.text= "Introduce un numero Valido"
+            }
         }
     }
-    private fun CalcularFactorial(numero : Int?) {
-
-
+    private fun CalcularFactorial(numero : Int) : Int{
+        var resultado =1
+        for (i in 1..numero){
+            resultado*=i
+        }
+    return resultado
     }
 }
