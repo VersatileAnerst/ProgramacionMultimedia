be0b3aea945b4ce366ec5bc7e87daf08314f1627
VersatileAnerst <151840641+VersatileAnerst@users.noreply.github.com>
2026-01-13 23:18:57 +0100

    Update color schemes

--- AppImagenes/app/src/main/java/com/example/appimagenes/ui/theme/Theme.kt
+++ AppImagenes/app/src/main/java/com/example/appimagenes/ui/theme/Theme.kt
@@ -12,15 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = MaterialOrange500,
+    secondary = MaterialCyan600,
+    tertiary = MaterialGrey800
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = MaterialOrange800,
+    secondary = MaterialTeal600,
+    tertiary = MaterialGrey800
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,7 +37,7 @@ private val LightColorScheme = lightColorScheme(
 fun AppImagenesTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false, //Colores del sistema
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
